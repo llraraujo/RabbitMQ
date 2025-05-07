61889698f614ed5ee82ca9970396ec6069e6dbff
llraraujo <lucasregoaraujo@gmail.com>
2025-05-07 09:11:05 -0300

    solution createad

---
+++ RabbitMq.Producer/Program.cs
@@ -0,0 +1,13 @@
+﻿using RabbitMQ.Client;
+using System.Threading.Tasks;
+
+namespace RabbitMq.Producer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+        }
+    }
+}
